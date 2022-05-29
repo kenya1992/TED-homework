2b9f65cdbd167d2b1dedb9a11b3792b8fd27bc11
kenya1992 <106466660+kenya1992@users.noreply.github.com>
2022-05-29 17:52:00 +0900

    Add files via upload

---
+++ 練習.py
@@ -0,0 +1,18 @@
+with open('hw.txt') as f:
+
+    data = f.read()
+
+
+words = {}
+
+for word in data.split():
+
+
+    words[word] = words.get(word, 0) + 1 
+
+
+d = [(v, k) for k, v in words.items()]
+d.sort()
+d.reverse()
+for count, word in d[:100]:
+    print(count, word)
